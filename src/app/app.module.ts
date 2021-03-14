4afa91f2e1c4b3feffcc4338799aa6044bb77d3b
Hajer-Ben-Mahmoud-Dev <80288233+Hajer-Ben-Mahmoud-Dev@users.noreply.github.com>
2021-03-14 12:38:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
+const route:Routes=[
+  {path:"", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"skills", component:SkillsComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
